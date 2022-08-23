4dbe604dac5894510b9011a1fca40622d4a3da9d
V-laz <Lazaruk.vitali@gmail.com>
2022-08-23 11:56:42 +0200

    create component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+return (
+      <header className='header'>        
+        <img src="https://all-t-shirts.ru/goods_images/ru126954II000e96164cc766b1c37cb013c9fb2fb31c9.jpg" alt="logo" />
+      </header>
+);
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+  
+  
+const Profile = () => {
+  return (
+
+      <div className='content'>
+        <div className="bodyHeader">
+          <img src="https://cdn.vox-cdn.com/thumbor/g67BVRfF03xzo-yOTwunt1HtAPg=/0x0:2550x1700/1200x800/filters:focal(1071x646:1479x1054)/cdn.vox-cdn.com/uploads/chorus_image/image/59517127/My_Post__6_.0.jpg" alt="fonImg" />
+        </div>
+        <div className="contentdescription">
+          <div>
+            ava + description
+          </div>
+          <div>My post
+            <div>New post</div>
+
+          </div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
+
+  );
+}
+
+export default Profile;
