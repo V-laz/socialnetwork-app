d2925a4c14fcddb2b7dc6a6a1f0590e57957a314
V-laz <Lazaruk.vitali@gmail.com>
2022-08-25 09:56:08 +0200

    add component message for dialog

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,29 +3,31 @@ import css from './Dialogs.module.css';
 import { NavLink } from 'react-router-dom';
 
 const DialogItem = (props) => {
-  let path = "/dialogs/"+props.id;
+  let path = "/dialogs/" + props.id;
   return (
     <div className={css.dialog}>
-    <NavLink to={path}>{props.name}</NavLink>
-  </div>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
   )
 }
-
+const Message = (props) => {
+  return (
+    <div className={css.dialog}>{props.message}</div>
+  )
+}
 const Dialogs = () => {
   return (
     <div className={css.dialogs}>
       <div className={css.dialogsItems}>
 
-        <DialogItem name="Gyver" id="1"/>
-        <DialogItem name="Lara" id="2"/>
-        <DialogItem name="Alex" id="33"/>
-
-
+        <DialogItem name="Gyver" id="1" />
+        <DialogItem name="Lara" id="2" />
+        <DialogItem name="Alex" id="33" />
       </div>
       <div className={css.messages}>
-        <div className={css.message}>hello</div>
-        <div className={css.message}>hello</div>
-        <div className={css.message}>hello</div>
+        <Message message="hi" />
+        <Message message="hello" />
+        <Message message="halahula" />
       </div>
     </div>
 
