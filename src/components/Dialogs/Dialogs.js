ab5f5a6fcdfce9564dc78d04375396fb31f40318
V-laz <Lazaruk.vitali@gmail.com>
2022-08-26 14:05:53 +0200

    adding data to the side Dialog page

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -16,13 +16,18 @@ const Message = (props) => {
   )
 }
 const Dialogs = () => {
+  let dialogData= [
+    {id:1, name:"Gyver"},
+    {id:2, name:"lara"},
+    {id:3, name:"Alex"}
+  ]
   return (
     <div className={css.dialogs}>
       <div className={css.dialogsItems}>
 
-        <DialogItem name="Gyver" id="1" />
-        <DialogItem name="Lara" id="2" />
-        <DialogItem name="Alex" id="33" />
+        <DialogItem name={dialogData[0].name} id={dialogData[0].id} />
+        <DialogItem name={dialogData[1].name} id={dialogData[1].id} />
+        <DialogItem name={dialogData[2].name} id={dialogData[2].id} />
       </div>
       <div className={css.messages}>
         <Message message="hi" />
