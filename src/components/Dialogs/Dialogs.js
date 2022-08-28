4be4f869e337ca90fe70d5a8d3ffd26c55712451
V-laz <Lazaruk.vitali@gmail.com>
2022-08-28 11:01:27 +0200

    add map metod for Message and dialog

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -16,29 +16,34 @@ const Message = (props) => {
   )
 }
 const Dialogs = () => {
-  let dialogData= [
-    {id:1, name:"Gyver"},
-    {id:2, name:"lara"},
-    {id:3, name:"Alex"}
+  let dialogs = [
+    { id: 1, name: "Gyver" },
+    { id: 2, name: "lara" },
+    { id: 3, name: "Alex" }
   ]
-  const messagesData = [
-    {id:1, message:"hi"},
-    {id:2, message:'hello'},
-    {id:3, message:"Yo"}
-    
+  const messages = [
+    { id: 1, message: "hi" },
+    { id: 2, message: 'hello' },
+    { id: 3, message: "Yo" }
+
   ]
+  let dialogsElements = dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />)
+  /* 
+  dialogsElements = dialogData.map(dialog => <DialogItem name={dialogData[0].name} id={dialogData[0].id} />)
+  [
+     <DialogItem name={dialogData[0].name} id={dialogData[0].id} />,
+     <DialogItem name={dialogData[1].name} id={dialogData[1].id} />,
+     <DialogItem name={dialogData[2].name} id={dialogData[2].id} />,
+   ]
+   */
+  let messagesElement = messages.map(message => <Message message={message.message} />)
   return (
     <div className={css.dialogs}>
       <div className={css.dialogsItems}>
-
-        <DialogItem name={dialogData[0].name} id={dialogData[0].id} />
-        <DialogItem name={dialogData[1].name} id={dialogData[1].id} />
-        <DialogItem name={dialogData[2].name} id={dialogData[2].id} />
+        {dialogsElements}
       </div>
       <div className={css.messages}>
-        <Message message={messagesData[0].message}/>
-        <Message message={messagesData[1].message}/>
-        <Message message={messagesData[2].message}/>
+        {messagesElement}
       </div>
     </div>
 
