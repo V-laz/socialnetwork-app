c1f3787a526303210d68f8098124506f5c59dd92
V-laz <Lazaruk.vitali@gmail.com>
2022-08-28 20:38:16 +0200

    active link dialog

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -5,8 +5,9 @@ import { NavLink } from 'react-router-dom';
 const DialogItem = (props) => {
   let path = "/dialogs/" + props.id;
   return (
-    <div className={css.dialog}>
-      <NavLink to={path}>{props.name}</NavLink>
+    <div className={css.dialogName}>
+      <NavLink to={path} className={dialogData => dialogData.isActive ? css.active : css.dialog}>{props.name}</NavLink>
+
     </div>
   )
 }
