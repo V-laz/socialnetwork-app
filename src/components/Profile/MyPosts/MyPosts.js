baa8bc8e17af3f17b93f59a6271d0e8f472b88ac
V-laz <Lazaruk.vitali@gmail.com>
2022-08-31 00:01:05 +0200

    MyPost: createRef for textarea,  add onclick for button; add addPost in state and MyPost, adding addPost in MyPosts via props

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Post from './Post/Post';
 import css from './MyPosts.module.css';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 
 
 const MyPosts = (props) => {
+  
+  let newPostElement=React.createRef();
+
+  let addPost =()=>{
+   let text =newPostElement.current.value;
+   props.addPost(text);
+  }
 
   let postsElements = props.posts.map(post => <Post message={post.message} likesCount={post.likesCount} />)
   return (
@@ -11,8 +19,8 @@ const MyPosts = (props) => {
     <div className={css.myPosts}>
       My posts
       <div className={css.textInfo}>
-        <textarea></textarea>
-        <button className={css.postButton}>Add post</button>
+        <textarea ref={newPostElement} ></textarea>
+        <button onClick={addPost} className={css.postButton}>Add post</button>
       </div>
       <div className={css.posts}>
         {postsElements}
